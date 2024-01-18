8e4a3d6a0f1bf410d147b97b32b03d932a6959b9
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2024-01-18 15:38:17 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -84,7 +84,7 @@ async def get_thumb(videoid):
             background = enhancer.enhance(0.6)
             image2 = background
                                                                                             
-            circle = Image.open("AshishX/assets/circle.png")
+            circle = Image.open("AnonXMusic/assets/circle.png")
 
             # changing circle color
             im = circle
@@ -116,13 +116,13 @@ async def get_thumb(videoid):
             image2.paste(circle, (0,0), mask = circle)
 
             # fonts
-            font1 = ImageFont.truetype('AshishX/assets/font.ttf', 30)
-            font2 = ImageFont.truetype('AshishX/assets/font2.ttf', 70)
-            font3 = ImageFont.truetype('AshishX/assets/font2.ttf', 40)
-            font4 = ImageFont.truetype('AshishX/assets/font2.ttf', 35)
+            font1 = ImageFont.truetype('AnonXMusic/assets/font.ttf', 30)
+            font2 = ImageFont.truetype('AnonXMusic/assets/font2.ttf', 70)
+            font3 = ImageFont.truetype('AnonXMusic/assets/font2.ttf', 40)
+            font4 = ImageFont.truetype('AnonXMusic/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "ASHISH MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "NOTHING MUSIC", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
